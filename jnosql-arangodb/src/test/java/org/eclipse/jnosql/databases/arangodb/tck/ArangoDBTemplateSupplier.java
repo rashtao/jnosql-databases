0d00e49c36cdbe1195afcff4738ff8a50215aa1b
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-11 07:39:09 +0000

    feat: create supplier to main
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-arangodb/src/test/java/org/eclipse/jnosql/databases/arangodb/tck/ArangoDBTemplateSupplier.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.arangodb.tck;
+
+import jakarta.enterprise.inject.se.SeContainerInitializer;
+import jakarta.enterprise.inject.spi.CDI;
+import jakarta.nosql.Template;
+import jakarta.nosql.tck.TemplateSupplier;
+import org.eclipse.jnosql.databases.arangodb.communication.ArangoDBConfigurations;
+import org.eclipse.jnosql.databases.arangodb.communication.DocumentDatabase;
+import org.eclipse.jnosql.mapping.core.config.MappingConfigurations;
+import org.eclipse.jnosql.mapping.document.DocumentTemplate;
+
+public class ArangoDBTemplateSupplier implements TemplateSupplier {
+
+    static {
+        System.setProperty(ArangoDBConfigurations.HOST.get() + ".1", DocumentDatabase.INSTANCE.host());
+        System.setProperty(MappingConfigurations.DOCUMENT_DATABASE.get(), "jakarta-nosql-tck");
+        SeContainerInitializer.newInstance().initialize();
+    }
+
+    @Override
+    public Template get() {
+        return CDI.current().select(DocumentTemplate.class).get();
+    }
+}
