6b2e7796a5cd5d586bf8a688b44dbbbf9261eaf6
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-17 19:09:59 +0000

    style remove unsed imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-elasticsearch/src/test/java/org/eclipse/jnosql/databases/elasticsearch/communication/QueryConverterTest.java
+++ jnosql-elasticsearch/src/test/java/org/eclipse/jnosql/databases/elasticsearch/communication/QueryConverterTest.java
@@ -32,7 +32,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
