1098377436deeca093ee372b97e64cdfdf87e3f4
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-20 19:06:51 +0000

    feat: update neo4j configuration
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JConfiguration.java
@@ -0,0 +1,33 @@
+/*
+ *
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.eclipse.jnosql.databases.neo4j.communication;
+
+import org.eclipse.jnosql.communication.Settings;
+import org.eclipse.jnosql.communication.semistructured.DatabaseConfiguration;
+import org.eclipse.jnosql.communication.semistructured.DatabaseManagerFactory;
+
+import java.util.logging.Logger;
+
+public class Neo4JConfiguration implements DatabaseConfiguration {
+
+    private static final Logger LOGGER = Logger.getLogger(Neo4JConfiguration.class.getName());
+
+    @Override
+    public DatabaseManagerFactory apply(Settings settings) {
+        return null;
+    }
+}
