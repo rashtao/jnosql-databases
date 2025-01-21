635501918a33a8dac1040dea18dfb9135fc5c5df
Otavio Santana <otaviopolianasantana@gmail.com>
2025-01-21 20:15:12 +0000

    feat: create initial structure to manager factory
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JDatabaseManagerFactory.java
+++ jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JDatabaseManagerFactory.java
@@ -1,4 +1,26 @@
 package org.eclipse.jnosql.databases.neo4j.communication;
 
-public class Neo4JDatabaseManagerFactory {
+import org.eclipse.jnosql.communication.semistructured.DatabaseManager;
+import org.eclipse.jnosql.communication.semistructured.DatabaseManagerFactory;
+
+import java.util.Objects;
+
+public class Neo4JDatabaseManagerFactory implements DatabaseManagerFactory {
+
+    private final Neo4Property property;
+
+    Neo4JDatabaseManagerFactory(Neo4Property property) {
+        this.property = property;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public DatabaseManager apply(String database) {
+        Objects.requireNonNull(database, "database is required");
+        return null;
+    }
 }
