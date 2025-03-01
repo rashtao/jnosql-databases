356825de59b2313f52619927f7bb5272cca55f5a
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-01 17:10:17 +0000

    docs: create documentation to neo4j
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/package-info.java
+++ jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/package-info.java
@@ -1,1 +1,25 @@
+/*
+ *
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+
+/**
+ * This package contains the communication layer for Neo4j integration with Eclipse JNoSQL.
+
+ * It provides implementations for managing communication with a Neo4j database,
+ * including inserting, updating, deleting, and querying entities. Additionally,
+ * it supports handling relationships (edges) between nodes and executing custom Cypher queries.
+ */
 package org.eclipse.jnosql.databases.neo4j.communication;
