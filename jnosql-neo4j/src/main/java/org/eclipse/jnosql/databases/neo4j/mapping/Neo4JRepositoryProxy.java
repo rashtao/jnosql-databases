01dce5a8efae720eabfda6950ac0c4f9f5e9f352
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-01 19:19:14 +0000

    feat: implement neo4j proxy repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/mapping/Neo4JRepositoryProxy.java
@@ -0,0 +1,110 @@
+/*
+ *  Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.neo4j.mapping;
+
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.core.query.AbstractRepository;
+import org.eclipse.jnosql.mapping.core.repository.DynamicReturn;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.semistructured.query.AbstractSemiStructuredRepositoryProxy;
+import org.eclipse.jnosql.mapping.semistructured.query.SemiStructuredRepositoryProxy;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.eclipse.jnosql.mapping.core.repository.DynamicReturn.toSingleResult;
+
+class Neo4JRepositoryProxy <T, K> extends AbstractSemiStructuredRepositoryProxy<T, K> {
+
+    private final Class<T> typeClass;
+
+    private final Neo4JTemplate template;
+
+    private final AbstractRepository<T,K> repository;
+
+    private final Converters converters;
+
+    private final EntityMetadata entityMetadata;
+
+    private final Class<?> repositoryType;
+
+    Neo4JRepositoryProxy(Neo4JTemplate template, Class<?> repositoryType,
+                             Converters converters, EntitiesMetadata entitiesMetadata) {
+
+        this.template = template;
+        this.typeClass = Class.class.cast(ParameterizedType.class.cast(repositoryType.getGenericInterfaces()[0])
+                .getActualTypeArguments()[0]);
+
+        this.converters = converters;
+        this.entityMetadata = entitiesMetadata.get(typeClass);
+        this.repositoryType = repositoryType;
+        this.repository = SemiStructuredRepositoryProxy.SemiStructuredRepository.of(template, entityMetadata);
+    }
+
+    @Override
+    protected AbstractRepository<T, K> repository() {
+        return repository;
+    }
+
+    @Override
+    protected Converters converters() {
+        return converters;
+    }
+
+    @Override
+    protected Class<?> repositoryType() {
+        return repositoryType;
+    }
+
+    @Override
+    protected EntityMetadata entityMetadata() {
+        return entityMetadata;
+    }
+
+    @Override
+    protected Neo4JTemplate template() {
+        return template;
+    }
+
+    @Override
+    public Object invoke(Object instance, Method method, Object[] args) throws Throwable {
+
+        Cypher cql = method.getAnnotation(Cypher.class);
+        if (Objects.nonNull(cql)) {
+
+            Stream<T> result;
+            Map<String, Object> values = CQLObjectUtil.getValues(args, method);
+            if (!values.isEmpty()) {
+                result = template.cql(cql.value(), values);
+            } else if (args == null || args.length == 0) {
+                result = template.cql(cql.value());
+            } else {
+                result = template.cql(cql.value(), args);
+            }
+            return DynamicReturn.builder()
+                    .classSource(typeClass)
+                    .methodSource(method)
+                    .result(() -> result)
+                    .singleResult(toSingleResult(method).apply(() -> result))
+                    .build().execute();
+        }
+
+        return super.invoke(instance, method, args);
+    }
+}
