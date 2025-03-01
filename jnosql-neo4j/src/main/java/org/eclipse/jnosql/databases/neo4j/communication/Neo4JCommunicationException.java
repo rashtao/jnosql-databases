218c9c6b660083de839e1042ac09998cfeb0d3a3
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-01 08:07:09 +0000

    docs: generate documentation to Neo4j
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JCommunicationException.java
+++ jnosql-neo4j/src/main/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JCommunicationException.java
@@ -18,6 +18,21 @@ package org.eclipse.jnosql.databases.neo4j.communication;
 
 import org.eclipse.jnosql.communication.CommunicationException;
 
+/**
+ * Exception representing general communication errors when interacting with a Neo4j database.
+ * <p>
+ * This exception is used as a base for more specific exceptions related to Neo4j operations,
+ * such as Cypher syntax errors, transaction failures, or connectivity issues.
+ * </p>
+ *
+ * <h3>Common Causes:</h3>
+ * <ul>
+ *     <li>Invalid Cypher query syntax.</li>
+ *     <li>Transaction failures during read or write operations.</li>
+ *     <li>Connection timeouts or authentication failures.</li>
+ *     <li>Inconsistent data constraints within the graph.</li>
+ * </ul>
+ */
 public class Neo4JCommunicationException extends CommunicationException {
 
     public Neo4JCommunicationException(String message) {
