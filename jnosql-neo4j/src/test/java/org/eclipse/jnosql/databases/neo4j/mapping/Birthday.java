4c5364350e3d876de5a52572b73f65239dfe169a
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-01 18:05:02 +0000

    test: create initial structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/mapping/Birthday.java
@@ -0,0 +1,70 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.databases.neo4j.mapping;
+
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+import java.util.Objects;
+
+@Entity
+public class Birthday {
+
+    @Id
+    private String name;
+
+    @Column
+    private Integer age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Birthday(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Birthday() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Birthday birthday = (Birthday) o;
+        return Objects.equals(name, birthday.name) &&
+                Objects.equals(age, birthday.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/mapping/Neo4JTemplateTest.java
@@ -0,0 +1,31 @@
+package org.eclipse.jnosql.databases.neo4j.mapping;
+
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
+import org.eclipse.jnosql.mapping.reflection.Reflections;
+import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@EnableAutoWeld
+@AddPackages(value = {Converters.class, EntityConverter.class,  Neo4JTemplate.class})
+@AddPackages(Music.class)
+@AddPackages(Reflections.class)
+@AddExtensions({EntityMetadataExtension.class})
+class Neo4JTemplateTest {
+
+    @Inject
+    private Neo4JTemplate template;
+
+    @Test
+    void shouldInjectMongoDBTemplate() {
+        Assertions.assertNotNull(template);
+    }
+}
