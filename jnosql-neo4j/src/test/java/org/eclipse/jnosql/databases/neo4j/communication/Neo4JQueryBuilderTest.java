205e27be050925842b646599d11303469b39c497
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-01 20:46:41 +0000

    test: update builder neo4j
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JQueryBuilderTest.java
+++ jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/communication/Neo4JQueryBuilderTest.java
@@ -32,7 +32,6 @@ import static org.mockito.Mockito.when;
 
 class Neo4JQueryBuilderTest {
 
-
     @Test
     void shouldBuildDeleteQueryForNode() {
         DeleteQuery query = mock(DeleteQuery.class);
@@ -63,6 +62,7 @@ class Neo4JQueryBuilderTest {
     void shouldBuildSelectQueryWithCondition() {
         SelectQuery query = mock(SelectQuery.class);
         when(query.name()).thenReturn("Person");
+
         CriteriaCondition condition = mock(CriteriaCondition.class);
         Element element = mock(Element.class);
         when(condition.element()).thenReturn(element);
@@ -91,4 +91,26 @@ class Neo4JQueryBuilderTest {
 
         assertThat(cypher).isEqualTo("MATCH (e:Person) RETURN e.name, e.age");
     }
+
+    @Test
+    void shouldTranslateIdToElementId() {
+        SelectQuery query = mock(SelectQuery.class);
+        when(query.name()).thenReturn("Person");
+
+        CriteriaCondition condition = mock(CriteriaCondition.class);
+        Element element = mock(Element.class);
+        when(condition.element()).thenReturn(element);
+        when(element.name()).thenReturn("_id");
+        when(element.get()).thenReturn("12345");
+        when(condition.condition()).thenReturn(org.eclipse.jnosql.communication.Condition.EQUALS);
+        when(query.condition()).thenReturn(java.util.Optional.of(condition));
+        when(query.columns()).thenReturn(List.of("name", "_id"));
+
+        Map<String, Object> parameters = new HashMap<>();
+        String cypher = Neo4JQueryBuilder.INSTANCE.buildQuery(query, parameters);
+
+        assertThat(cypher).isEqualTo("MATCH (e:Person) WHERE elementId(e) = $id RETURN e.name, elementId(e)");
+        assertThat(parameters).containsEntry("id", "12345");
+    }
+
 }
