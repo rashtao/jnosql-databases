9105641740b6a264e4acbfd132ad5ee0d61ab0e5
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-02 04:40:54 +0000

    docs: enhance CQl documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-cassandra/src/main/java/org/eclipse/jnosql/databases/cassandra/mapping/CQL.java
+++ jnosql-cassandra/src/main/java/org/eclipse/jnosql/databases/cassandra/mapping/CQL.java
@@ -20,14 +20,25 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation used to define a dynamic CQL query method in CassandraRepository and CassandraRepositoryAsync interfaces.
+ * Annotation used to define a dynamic CQL query method in {@link CassandraRepository}.
+ * <p>
+ * Methods annotated with {@code @CQL} allow the execution of custom Cassandra Query Language (CQL) statements
+ * within repository interfaces.
+ * </p>
+ * <p>
+ * Example usage:
+ * <pre>{@code
+ * @CQL("SELECT * FROM users WHERE username = :username")
+ * List<User> findByUsername(@Param("username") String username);
+ * }</pre>
+ * </p>
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface CQL {
 
     /**
-     * The CQL query string.
+     * The CQL query string to be executed.
      *
      * @return the CQL query string
      */
