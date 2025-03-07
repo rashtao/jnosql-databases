e5623fde6710d757471161689998bb948d2297aa
Otavio Santana <otaviopolianasantana@gmail.com>
2025-03-07 07:38:09 +0000

    feat: update  graphdatabas configuration
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-tinkerpop/src/test/java/org/eclipse/jnosql/databases/tinkerpop/mapping/GraphTemplateTest.java
+++ jnosql-tinkerpop/src/test/java/org/eclipse/jnosql/databases/tinkerpop/mapping/GraphTemplateTest.java
@@ -16,6 +16,7 @@ package org.eclipse.jnosql.databases.tinkerpop.mapping;
 
 import jakarta.data.exceptions.EmptyResultException;
 import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
 import org.eclipse.jnosql.communication.Value;
 import org.eclipse.jnosql.communication.semistructured.Element;
 import org.eclipse.jnosql.databases.tinkerpop.mapping.entities.Human;
@@ -23,6 +24,7 @@ import org.eclipse.jnosql.databases.tinkerpop.mapping.entities.Magazine;
 import org.eclipse.jnosql.databases.tinkerpop.mapping.spi.GraphExtension;
 import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
+import org.eclipse.jnosql.mapping.graph.GraphTemplate;
 import org.eclipse.jnosql.mapping.reflection.Reflections;
 import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
 import org.jboss.weld.junit5.auto.AddExtensions;
@@ -46,315 +48,22 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @AddPackages(GraphProducer.class)
 @AddPackages(Reflections.class)
 @AddExtensions({EntityMetadataExtension.class, GraphExtension.class})
-class EdgeEntityTest {
+class GraphTemplateTest {
 
 
     @Inject
-    private TinkerpopTemplate tinkerpopTemplate;
+    private GraphTemplate template;
 
 
     @Test
     void shouldReturnErrorWhenInboundIsNull() {
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            Human human = Human.builder().withName("Poliana").withAge().build();
-            Magazine magazine = null;
-            tinkerpopTemplate.edge(human, "reads", magazine);
+        var human = template.insert(Human.builder().withName("Poliana").withAge().build());
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(human).isNotNull();
+            soft.assertThat(human.getName()).isEqualTo("Poliana");
+            soft.assertThat(human.getAge()).isNotNull();
         });
-    }
-
-    @Test
-    void shouldReturnErrorWhenOutboundIsNull() {
-        Assertions.assertThrows(IllegalStateException.class, () -> {
-            Human human = Human.builder().withName("Poliana").withAge().build();
-            Magazine magazine = Magazine.builder().withAge(2007).withName("The Shack").build();
-            tinkerpopTemplate.edge(human, "reads", magazine);
-        });
-    }
-
-    @Test
-    void shouldReturnErrorWhenLabelIsNull() {
-        Assertions.assertThrows(NullPointerException.class, () -> {
-            Human human = Human.builder().withName("Poliana").withAge().build();
-            Magazine magazine = Magazine.builder().withAge(2007).withName("The Shack").build();
-            tinkerpopTemplate.edge(human, (String) null, magazine);
-        });
-    }
-
-    @Test
-    void shouldReturnNullWhenInboundIdIsNull() {
-        Assertions.assertThrows(EmptyResultException.class, () -> {
-            Human human = Human.builder().withId(-5).withName("Poliana").withAge().build();
-            Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-            tinkerpopTemplate.edge(human, "reads", magazine);
-        });
-
-    }
-
-    @Test
-    void shouldReturnNullWhenOutboundIdIsNull() {
-        Assertions.assertThrows(IllegalStateException.class, () -> {
-            Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-            Magazine magazine = Magazine.builder().withAge(2007).withName("The Shack").build();
-            tinkerpopTemplate.edge(human, "reads", magazine);
-        });
-    }
-
-    @Test
-    void shouldReturnEntityNotFoundWhenOutBoundDidNotFound() {
-        Assertions.assertThrows( EmptyResultException.class, () -> {
-            Human human = Human.builder().withId(-10L).withName("Poliana").withAge().build();
-            Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-            tinkerpopTemplate.edge(human, "reads", magazine);
-        });
-    }
-
-    @Test
-    void shouldReturnEntityNotFoundWhenInBoundDidNotFound() {
-        Assertions.assertThrows( EmptyResultException.class, () -> {
-            Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-            Magazine magazine = Magazine.builder().withId(10L).withAge(2007).withName("The Shack").build();
-            tinkerpopTemplate.edge(human, "reads", magazine);
-        });
-    }
-
-    @Test
-    void shouldCreateAnEdge() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        assertEquals("reads", edge.label());
-        assertEquals(human, edge.outgoing());
-        assertEquals(magazine, edge.incoming());
-        assertTrue(edge.isEmpty());
-        assertNotNull(edge.id());
-    }
-
-    @Test
-    void shouldGetId() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        assertEquals("reads", edge.label());
-        assertEquals(human, edge.outgoing());
-        assertEquals(magazine, edge.incoming());
-        assertTrue(edge.isEmpty());
-        assertNotNull(edge.id());
-        final Long id = edge.id(Long.class);
-        assertNotNull(id);
-
-        assertEquals(id, edge.id(Integer.class).longValue());
-
-    }
-
-    @Test
-    void shouldCreateAnEdgeWithSupplier() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, () -> "reads", magazine);
-
-        assertEquals("reads", edge.label());
-        assertEquals(human, edge.outgoing());
-        assertEquals(magazine, edge.incoming());
-        assertTrue(edge.isEmpty());
-        assertNotNull(edge.id());
-    }
-
-    @Test
-    void shouldUseAnEdge() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        EdgeEntity sameEdge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        assertEquals(edge.id(), sameEdge.id());
-        assertEquals(edge, sameEdge);
-    }
-
-    @Test
-    void shouldUseAnEdge2() {
-        Human poliana = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Human nilzete = tinkerpopTemplate.insert(Human.builder().withName("Nilzete").withAge().build());
-
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(poliana, "reads", magazine);
-        EdgeEntity edge1 = tinkerpopTemplate.edge(nilzete, "reads", magazine);
-
-        EdgeEntity sameEdge = tinkerpopTemplate.edge(poliana, "reads", magazine);
-        EdgeEntity sameEdge1 = tinkerpopTemplate.edge(nilzete, "reads", magazine);
-
-        assertEquals(edge.id(), sameEdge.id());
-        assertEquals(edge, sameEdge);
-
-        assertEquals(edge1.id(), sameEdge1.id());
-        assertEquals(edge1, sameEdge1);
-
-    }
-
-    @Test
-    void shouldUseADifferentEdge() {
-        Human poliana = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Human nilzete = tinkerpopTemplate.insert(Human.builder().withName("Nilzete").withAge().build());
-
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(poliana, "reads", magazine);
-        EdgeEntity edge1 = tinkerpopTemplate.edge(nilzete, "reads", magazine);
-
-        EdgeEntity sameEdge = tinkerpopTemplate.edge(poliana, "reads", magazine);
-        EdgeEntity sameEdge1 = tinkerpopTemplate.edge(nilzete, "reads", magazine);
-
-        assertNotEquals(edge.id(), edge1.id());
-        assertNotEquals(edge.id(), sameEdge1.id());
-
-        assertNotEquals(sameEdge1.id(), sameEdge.id());
-    }
-
-    @Test
-    void shouldReturnErrorWhenAddKeyIsNull() {
-        assertThrows(NullPointerException.class, () -> {
-            Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-            Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-            EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-            edge.add(null, "Brazil");
-        });
-    }
-
-    @Test
-    void shouldReturnErrorWhenAddValueIsNull() {
-
-        assertThrows(NullPointerException.class, () -> {
-            Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-            Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-            EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-            edge.add("where", null);
-        });
-    }
-
-    @Test
-    void shouldAddProperty() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-        edge.add("where", "Brazil");
-
-        assertFalse(edge.isEmpty());
-        assertEquals(1, edge.size());
-        assertThat(edge.properties()).contains(Element.of("where", "Brazil"));
-    }
-
-    @Test
-    void shouldAddPropertyWithValue() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-        edge.add("where", Value.of("Brazil"));
-
-        assertFalse(edge.isEmpty());
-        assertEquals(1, edge.size());
-        assertThat(edge.properties()).contains(Element.of("where", "Brazil"));
-    }
-
-
-    @Test
-    void shouldReturnErrorWhenRemoveNullKeyProperty() {
-        assertThrows(NullPointerException.class, () -> {
-            Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-            Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-            EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-            edge.add("where", "Brazil");
-
-
-            assertFalse(edge.isEmpty());
-            edge.remove(null);
-        });
-    }
-
-    @Test
-    void shouldRemoveProperty() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-        edge.add("where", "Brazil");
-        assertEquals(1, edge.size());
-        assertFalse(edge.isEmpty());
-        edge.remove("where");
-        assertTrue(edge.isEmpty());
-        assertEquals(0, edge.size());
-    }
-
-    @Test
-    void shouldFindProperty() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-        edge.add("where", "Brazil");
-
-        Optional<Value> where = edge.get("where");
-        assertTrue(where.isPresent());
-        assertEquals("Brazil", where.get().get());
-        assertFalse(edge.get("not").isPresent());
-
-    }
-
-    @Test
-    void shouldDeleteAnEdge() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-        edge.delete();
-
-        EdgeEntity newEdge = tinkerpopTemplate.edge(human, "reads", magazine);
-        assertNotEquals(edge.id(), newEdge.id());
-
-        tinkerpopTemplate.deleteEdge(newEdge.id());
-    }
-
-    @Test
-    void shouldReturnErrorWhenDeleteAnEdgeWithNull() {
-        assertThrows(NullPointerException.class, () -> tinkerpopTemplate.delete((Iterable<Object>) null));
-    }
-
-    @Test
-    void shouldDeleteAnEdge2() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        tinkerpopTemplate.deleteEdge(edge.id());
-
-        EdgeEntity newEdge = tinkerpopTemplate.edge(human, "reads", magazine);
-        assertNotEquals(edge.id(), newEdge.id());
-    }
-
-
-    @Test
-    void shouldReturnErrorWhenFindEdgeWithNull() {
-        assertThrows(NullPointerException.class, () -> tinkerpopTemplate.edge(null));
-    }
-
-
-    @Test
-    void shouldFindAnEdge() {
-        Human human = tinkerpopTemplate.insert(Human.builder().withName("Poliana").withAge().build());
-        Magazine magazine = tinkerpopTemplate.insert(Magazine.builder().withAge(2007).withName("The Shack").build());
-        EdgeEntity edge = tinkerpopTemplate.edge(human, "reads", magazine);
-
-        Optional<EdgeEntity> newEdge = tinkerpopTemplate.edge(edge.id());
-
-        assertTrue(newEdge.isPresent());
-        assertEquals(edge.id(), newEdge.get().id());
-
-        tinkerpopTemplate.deleteEdge(edge.id());
-    }
-
-    @Test
-    void shouldNotFindAnEdge() {
-        Optional<EdgeEntity> edgeEntity = tinkerpopTemplate.edge(-12L);
-
-        assertFalse(edgeEntity.isPresent());
     }
 
 }
