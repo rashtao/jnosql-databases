ab09a1cebe3d3d96667977b03ba7a0888a42ff03
Otavio Santana <otaviopolianasantana@gmail.com>
2025-05-18 21:38:25 +0100

    test: generate a magazine repository
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/integration/MagazineRepository.java
+++ jnosql-neo4j/src/test/java/org/eclipse/jnosql/databases/neo4j/integration/MagazineRepository.java
@@ -15,9 +15,19 @@
 
 package org.eclipse.jnosql.databases.neo4j.integration;
 
+import jakarta.data.repository.Param;
 import jakarta.data.repository.Repository;
+import org.eclipse.jnosql.databases.neo4j.mapping.Cypher;
 import org.eclipse.jnosql.databases.neo4j.mapping.Neo4JRepository;
+
+import java.util.List;
 
 @Repository
 public interface MagazineRepository extends Neo4JRepository<Magazine, String> {
+
+    @Cypher("MATCH (m:Magazine) RETURN m")
+    List<Magazine> findAllByCypher();
+
+    @Cypher("MATCH (m:Magazine{title: $title}) RETURN m")
+    List<Magazine> findByTitle(@Param("title") String title);
 }
