dbf5d3664df026ab35368b65a73b98fda22497eb
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-10 20:47:24 +0100

    docs: enhance documentation on Stringmatch
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-database-commons/src/main/java/org/eclipse/jnosql/communication/driver/StringMatch.java
+++ jnosql-database-commons/src/main/java/org/eclipse/jnosql/communication/driver/StringMatch.java
@@ -32,24 +32,58 @@ import java.util.Objects;
  */
 public enum StringMatch {
 
+    /**
+     * Exact match.
+     * <p>
+     * The given value will be used as-is, without adding any wildcards.
+     * For SQL, this corresponds to {@code column = 'value'}.
+     * </p>
+     */
     DEFAULT {
         @Override
         public String apply(String value) {
             return value;
         }
     },
+
+    /**
+     * Contains match.
+     * <p>
+     * The given value will be wrapped with wildcards on both sides:
+     * {@code %value%}. For SQL, this corresponds to
+     * {@code column LIKE '%value%'}.
+     * </p>
+     */
     CONTAINS {
         @Override
         public String apply(String value) {
             return "%" + value + "%";
         }
     },
+
+    /**
+     * Starts-with match.
+     * <p>
+     * The given value will be followed by a wildcard:
+     * {@code value%}. For SQL, this corresponds to
+     * {@code column LIKE 'value%'}.
+     * </p>
+     */
     STARTS_WITH {
         @Override
         public String apply(String value) {
             return value + "%";
         }
     },
+
+    /**
+     * Ends-with match.
+     * <p>
+     * The given value will be preceded by a wildcard:
+     * {@code %value}. For SQL, this corresponds to
+     * {@code column LIKE '%value'}.
+     * </p>
+     */
     ENDS_WITH {
         @Override
         public String apply(String value) {
@@ -57,10 +91,27 @@ public enum StringMatch {
         }
     };
 
+    /**
+     * Applies the match strategy to the given value, producing a pattern string.
+     *
+     * @param value the value to be transformed into a pattern
+     * @return the pattern string, with wildcards applied according to the match strategy
+     */
     public abstract String apply(String value);
 
+    /**
+     * Formats the given value by applying the match strategy.
+     * <p>
+     * This method ensures the value is not {@code null} before applying the strategy.
+     * </p>
+     *
+     * @param value the value to be transformed into a pattern
+     * @return the pattern string, with wildcards applied according to the match strategy
+     * @throws NullPointerException if {@code value} is {@code null}
+     */
     public String format(String value) {
         Objects.requireNonNull(value, "value cannot be null");
         return apply(value);
     }
+
 }
