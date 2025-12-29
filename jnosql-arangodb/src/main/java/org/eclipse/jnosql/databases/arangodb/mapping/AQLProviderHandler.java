0f5e80d783efc9217a62ada2a8ce72552c138f4b
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-29 22:54:08 +0000

    chore: update AQLProviderHandler to utilize RepositoryMetadataUtils for parameter extraction

--- jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/mapping/AQLProviderHandler.java
+++ jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/mapping/AQLProviderHandler.java
@@ -2,6 +2,7 @@ package org.eclipse.jnosql.databases.arangodb.mapping;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.jnosql.mapping.ProviderQuery;
+import org.eclipse.jnosql.mapping.core.repository.RepositoryMetadataUtils;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.ProviderQueryHandler;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
 
@@ -15,13 +16,15 @@ class AQLProviderHandler  implements ProviderQueryHandler {
     public <T> T execute(RepositoryInvocationContext context) {
         var method = context.method();
         var parameters = context.parameters();
+        var template = (ArangoDBTemplate) context.template();
 
         var sampleQueryProvider = method.annotations().stream()
                 .filter(annotation -> AQL.class.equals(annotation.annotation()))
                 .findFirst().orElseThrow();
 
         Map<String, Object> attributes = sampleQueryProvider.attributes();
-        String value = (String) attributes.get("value");
+        var aql = (String) attributes.get("value");
+        Map<String, Object> params = RepositoryMetadataUtils.INSTANCE.getParams(method, parameters);
         return null;
     }
 }
