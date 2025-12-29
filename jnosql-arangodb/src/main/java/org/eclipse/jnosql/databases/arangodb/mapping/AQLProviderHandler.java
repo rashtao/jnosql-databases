cedd692c489c32f4324ec294d9ca23f8048b24ba
Otavio Santana <otaviopolianasantana@gmail.com>
2025-12-29 22:47:17 +0000

    chore: enhance AQLProviderHandler to extract query attributes from context

--- jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/mapping/AQLProviderHandler.java
+++ jnosql-arangodb/src/main/java/org/eclipse/jnosql/databases/arangodb/mapping/AQLProviderHandler.java
@@ -5,12 +5,23 @@ import org.eclipse.jnosql.mapping.ProviderQuery;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.ProviderQueryHandler;
 import org.eclipse.jnosql.mapping.metadata.repository.spi.RepositoryInvocationContext;
 
+import java.util.Map;
+
 @ApplicationScoped
 @ProviderQuery("aql-query")
 class AQLProviderHandler  implements ProviderQueryHandler {
 
     @Override
     public <T> T execute(RepositoryInvocationContext context) {
+        var method = context.method();
+        var parameters = context.parameters();
+
+        var sampleQueryProvider = method.annotations().stream()
+                .filter(annotation -> AQL.class.equals(annotation.annotation()))
+                .findFirst().orElseThrow();
+
+        Map<String, Object> attributes = sampleQueryProvider.attributes();
+        String value = (String) attributes.get("value");
         return null;
     }
 }
